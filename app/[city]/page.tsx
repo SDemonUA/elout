446b9602d9f7e633e7cad7116f690513ff3f9a81
Dmitry Sokol <dmitry.sokol@pinetool.ai>
2025-12-04 21:20:49 +0200

    feat: add navigation link for city selection and display source URL in schedule

--- app/[city]/page.tsx
+++ app/[city]/page.tsx
@@ -2,15 +2,17 @@ import { supportedCities, getCityLabel, ScheduleProviderFactory } from "@/lib/sc
 
 import EmptySchedule from "@/components/empty-schedule";
 import Schedule from "@/components/schedule";
+import { ChevronLeft } from "lucide-react";
 
 import { Suspense } from "react";
+import Link from "next/link";
 
 export function generateStaticParams() {
   return supportedCities.map((city) => ({ city }));
 }
 
 export const dynamicParams = false;
-export const revalidate = 5 * 60;
+export const revalidate = 300;
 
 export async function generateMetadata({ params }: { params: Promise<{ city: string }> }) {
   const { city } = await params;
@@ -41,18 +43,17 @@ export default async function Page({ params }: { params: Promise<{ city: string 
     dateStyle: "long",
   });
 
+  const sourceUrl = new URL(schedule.url);
+
   return (
     <main className="max-w-3xl mx-auto p-4">
+      <div>
+        <Link href="/" className="flex items-center text-cyan-600 hover:text-cyan-800 ">
+          <ChevronLeft className="inline-block mr-2" />
+          <span>Обрати інше місто</span>
+        </Link>
+      </div>
       <div className="m-4 text-center">
-        <div
-          className="text-9xl"
-          style={{
-            fontFeatureSettings: '"ss05"',
-            fontVariantLigatures: "common-ligatures",
-          }}
-        >
-          F
-        </div>
         <div className="text-7xl">{cityLabel}</div>
       </div>
       <h2 className="text-2xl text-center font-semibold mb-1">
@@ -60,6 +61,16 @@ export default async function Page({ params }: { params: Promise<{ city: string 
       </h2>
       <p className="mb-4 text-sm text-center text-secondary-foreground">
         Оновлено: {scheduleUpdateDate}
+        <br />
+        Джерело:{" "}
+        <a
+          href={sourceUrl.toString()}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline hover:text-cyan-800"
+        >
+          {sourceUrl.hostname + sourceUrl.pathname}
+        </a>
       </p>
 
       <Suspense>
